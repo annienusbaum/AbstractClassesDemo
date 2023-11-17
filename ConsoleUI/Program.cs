ab3e9c5b5bd143fb1ea294b718275a96eafb5b7c
Annie Nusbaum <annie.nusbaum.83@gmail.com>
2023-11-17 12:21:49 -0500

    AbstractExercise

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -18,32 +18,57 @@ namespace ConsoleUI
             #region Vehicles
 
             /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties: Year, Make, and Model
-             * Set the defaults of the properties to something generic in the Vehicle class
-             * Vehicle class shall have an abstract method called DriveAbstract with no implementation.
-             * Vehicle class shall have a virtual method called DriveVirtual with a base implementation.
+             * VEHICHLE IS THE BASE CLASS FOR OTHER CLASSES
+             * DONE - Create an abstract class called Vehicle
+             * DONE - The vehicle class shall have three string properties: Year, Make, and Model
+             * DONE - Set the defaults of the properties to something generic in the Vehicle class
+             * DONE - Vehicle class shall have an abstract method called DriveAbstract with no implementation.
+             * DONE - Vehicle class shall have a virtual method called DriveVirtual with a base implementation.
              */
 
             /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distinct property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
+             * DONE - Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
+             * DONE -Add a distinct property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
+             * DONE -Provide the implementations for the abstract methods
+             * DONE Only in the Motorcycle class will you override the virtual drive method
             */
 
-            // Create a list of Vehicle called vehicles
+            // DONE- Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
+
+
 
             /*
-             * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
+             * DONE - Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * 
-             * Set the properties values with object initializer syntax
+             * DONE - Set the properties values with object initializer syntax
              */
 
+            Car focus = new Car() { HasTrunk = true, Make = "Ford", Model = "Focus", Year = 2013 };
+            Motorcycle motorcycle = new Motorcycle() { HasSideCart = true, Make = "Harley", Model = "Davidson", Year = 1980 };
+
+
+            Vehicle sedan = new Car() { HasTrunk = true, Make = "Subaru", Model = "Legacy", Year = 2023 };
+            Vehicle suv = new Car() { HasTrunk = false, Make = "Jeep", Model = "Ascent", Year = 2023 };
+
+
             /*
-             * Add the 4 vehicles to the list
+             * DONE - Add the 4 vehicles to the list
              * Using a foreach loop iterate through the list and display each of the properties
              */
+
+            vehicles.Add(focus);
+            vehicles.Add(motorcycle);
+            vehicles.Add(sedan);
+            vehicles.Add(suv);
+
+            foreach(var veh in vehicles)
+            {
+                Console.WriteLine($"Make {veh.Make} Model {veh.Model} Year {veh.Year}");
+                veh.DriveAbstract();
+                Console.WriteLine("------------------------");
+            }
+
 
             // Call each of the drive methods for one car and one motorcycle
 
@@ -52,3 +77,10 @@ namespace ConsoleUI
         }
     }
 }
+
+
+
+
+//vehicle vehicle1 = new Car();
+//calling abstract methods in the loop looks like
+//
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleUI
+{
+	public abstract class Vehicle
+	{
+		public int Year { get; set; } = 1;
+		public string Make { get; set; } = "Default Model";
+		public string Model { get; set; } = "Default Model";
+
+		public abstract void DriveAbstract(); //stubbed out method -has no implementation
+
+		public virtual void DriveVirtual()
+		{
+			Console.WriteLine($"This {GetType().Name} is virtually in drive");
+		}
+	}
+}
+
